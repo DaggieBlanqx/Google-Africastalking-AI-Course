9136d2a073d1bb376e0628c04677d8fb15cb1f66
Douglas Mwangi <douglas.mwangi@africastalking.com>
2025-09-30 03:33:44 +0300

    Refactor: Load environment variables and configure debug mode in app

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from routes import register_routes
 
-PORT = 9000
+import os
+from dotenv import load_dotenv
+
+# Load .env file into environment
+load_dotenv()
+
+PORT = int(os.getenv("PORT", 9000))  # default 5000 if not set
+
+DEBUG = os.getenv("FLASK_ENV", "production") == "development"
+
 app = Flask(__name__)
 register_routes(app)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT, debug=True)
+    app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
